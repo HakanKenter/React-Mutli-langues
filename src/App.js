68bed1e5709c1594f3ced28488374e31e271db05
Hakan Kenter <hakan.kt@outlook.fr>
2022-05-15 17:03:11 +0200

    Create Context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Contenu from './Components/Contenu/Contenu'
+import ToogleLangs from './Components/ToggleLangs/ToggleLangs'
+import ContextProvider from './context/langContext'
 
 function App() {
   return (
-    <>
-
-    </>
+    <ContextProvider>
+      <ToogleLangs />
+      <Contenu />
+    </ContextProvider>
   );
 }
 
